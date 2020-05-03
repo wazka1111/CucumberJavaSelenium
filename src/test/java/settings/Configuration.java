193fc03979a95e96fd93e8b173e1449d1684631c
Darek <hryciuk.d@gmail.com>
2020-05-03 20:35:38 +0200

    Update Configuration.java

--- src/test/java/settings/Configuration.java
+++ src/test/java/settings/Configuration.java
@@ -2,7 +2,9 @@ package settings;
 
 public class Configuration {
     public static final String BROWSER="CHROME";
-    public static final String CHROME_DRIVER_PATH="C:\\Users\\DH\\Desktop\\cucumberSeleniumDemo\\libs\\chromedriver.exe";
+    //public static final String CHROME_DRIVER_PATH="C:\\Users\\DH\\Desktop\\cucumberSeleniumDemo\\libs\\chromedriver.exe";//windows
+    public static final String CHROME_DRIVER_PATH="/var/lib/jenkins/workspace/SeleniumCucumberPipelinee_master/libs/chromedriver.exe";//linux
+    
     //Options: YES - headless mode, NO - normal mode with visible browser window.
     public static final String HEADLESS_BROWSER_MODE="NO";
     public static final int TIME_OUT_EXPLICITE=10;
